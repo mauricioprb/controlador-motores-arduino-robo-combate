bedb0f09d9d5bc3294e4fb10a916400aebef9f70
Mauricio Pereira <mauricio.pereira@ufn.edu.br>
2023-06-17 01:14:54 -0300

    Update controladorDC.cpp

--- controladorDC.cpp
+++ controladorDC.cpp
@@ -9,12 +9,23 @@ const int PINO_MOTOR2_A = 9;
 const int PINO_MOTOR2_B = 10;
 
 // Declaração das variáveis com valores iniciais
-int ch1 = 0;
-int ch2 = 0;
+int ch1_samples[5];  // Array para armazenar as amostras do canal 1
+int ch2_samples[5];  // Array para armazenar as amostras do canal 2
+int ch1_index = 0;  // Índice atual para as amostras do canal 1
+int ch2_index = 0;  // Índice atual para as amostras do canal 2
 int velocidade = 0;
 int curva = 0;
 int velocidade_motor1 = 0;
 int velocidade_motor2 = 0;
+
+// Função para calcular a média dos últimos N valores de um array
+int calcularMedia(int* samples, int index) {
+  int sum = 0;
+  for (int i = 0; i < 5; i++) {
+    sum += samples[i];
+  }
+  return sum / 5;
+}
 
 void setup() {
   Serial.begin(9600);
@@ -29,40 +40,48 @@ void setup() {
 
 void loop() {
   // Leitura dos pulsos dos canais de entrada
-  ch1 = pulseIn(PINO_CH1, HIGH, 50000); // 50 ms de timeout
-  ch2 = pulseIn(PINO_CH2, HIGH, 50000); // 50 ms de timeout
+  ch1_samples[ch1_index] = pulseIn(PINO_CH1, HIGH, 50000); // 50 ms de timeout
+  ch2_samples[ch2_index] = pulseIn(PINO_CH2, HIGH, 50000); // 50 ms de timeout
 
+  // Atualiza o índice das amostras circulando no array
+  ch1_index = (ch1_index + 1) % 5;
+  ch2_index = (ch2_index + 1) % 5;
 
   // Verifica se os pulsos são válidos antes de mapear
-  if (ch1 != 0 && ch2 != 0) {
+  if (ch1_samples[ch1_index] != 0 && ch2_samples[ch2_index] != 0) {
+    // Calcula a média dos últimos N valores
+    int ch1_filtered = calcularMedia(ch1_samples, ch1_index);
+    int ch2_filtered = calcularMedia(ch2_samples, ch2_index);
+
     // Mapeamento dos valores de 1000-2000 para a faixa de -255 a 255
-    velocidade = map(ch1, 1000, 2000, -255, 255);
-    curva = map(ch2, 1000, 2000, -255, 255);
+    velocidade = map(ch1_filtered, 1000, 2000, -255, 255);
+    curva = map(ch2_filtered, 1000, 2000, -255, 255);
 
     // Cálculo das velocidades dos motores com restrição de faixa
     velocidade_motor1 = constrain(velocidade - curva, -255, 255);
     velocidade_motor2 = constrain(velocidade + curva, -255, 255);
-  } else {
-    // Se os pulsos não são válidos, parar os motores
-    velocidade_motor1 = 0;
-    velocidade_motor2 = 0;
-  }
+    
+    // Controle do motor 1
+    if (velocidade_motor1 >= 0) {
+      analogWrite(PINO_MOTOR1_A, velocidade_motor1);
+      analogWrite(PINO_MOTOR1_B, 0);
+    } else {
+      analogWrite(PINO_MOTOR1_A, 0);
+      analogWrite(PINO_MOTOR1_B, -velocidade_motor1);
+    }
 
-  // Controle do motor 1
-  if (velocidade_motor1 >= 0) {
-    analogWrite(PINO_MOTOR1_A, velocidade_motor1);
-    analogWrite(PINO_MOTOR1_B, 0);
+    // Controle do motor 2
+    if (velocidade_motor2 >= 0) {
+      analogWrite(PINO_MOTOR2_A, velocidade_motor2);
+      analogWrite(PINO_MOTOR2_B, 0);
+    } else {
+      analogWrite(PINO_MOTOR2_A, 0);
+      analogWrite(PINO_MOTOR2_B, -velocidade_motor2);
+    }
   } else {
+    // Se os pulsos não são válidos, desligar os motores
     analogWrite(PINO_MOTOR1_A, 0);
-    analogWrite(PINO_MOTOR1_B, -velocidade_motor1);
-  }
-
-  // Controle do motor 2
-  if (velocidade_motor2 >= 0) {
-    analogWrite(PINO_MOTOR2_A, velocidade_motor2);
-    analogWrite(PINO_MOTOR2_B, 0);
-  } else {
+    analogWrite(PINO_MOTOR1_B, 0);
     analogWrite(PINO_MOTOR2_A, 0);
-    analogWrite(PINO_MOTOR2_B, -velocidade_motor2);
-  }
-}
+    analogWrite(PINO_MOTOR2_B, 0);
+  }}
